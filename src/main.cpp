a67fb13f6ffce5eff3de07379d90e5e8c9af8387
Guillaume Fournier-Mayer <guillaume.fm@mailbox.org>
2020-10-22 18:30:44 +0200

    Remove unsued code from Main

--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,6 @@
 
 #define DEBUG 1
 
-uint32_t get_pulse_length(level_t level) {
-  uint32_t cycles = 0;
-
-  uint8_t before = IS_BIT_SET(PIND, PD2);
-  while ((IS_BIT_SET(PIND, PD2) == level) && cycles < 160000000L) {
-    ++cycles;
-  }
-  // print_ln_debug("E: %u\tLB: %u\tLA: %u\t%u", level, before,
-  //  IS_BIT_SET(PIND, PD2), cycles);
-  return cycles;
-}
-
 int main() {
   Usart::init();
   DHT22 dht22(PD2);
